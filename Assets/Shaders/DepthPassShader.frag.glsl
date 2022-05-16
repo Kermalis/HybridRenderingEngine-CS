d5b4ca932baeaec07b1b67043bba2c6ce73c78f5
Kermalis <29823718+Kermalis@users.noreply.github.com>
2022-05-16 03:36:35 -0400

    Fix DepthPassShader

--- Assets/Shaders/DepthPassShader.frag.glsl
+++ Assets/Shaders/DepthPassShader.frag.glsl
@@ -2,11 +2,11 @@
 
 in vec2 TexCoords;
 
-uniform sampler2D albedo;
+uniform sampler2D albedoMap;
 
 void main(){
     //This kills perf but I have no idea of how else to alpha test the sponza leaves 
-    float alpha = texture(albedo, TexCoords).a;
+    float alpha = texture(albedoMap, TexCoords).a;
     if(alpha < 0.5){
         discard;
     }
